1083073fd487361b00d9a59fbc8c7c35e90ba14e
Dorovich <silverdovi25@gmail.com>
2024-02-29 20:52:42 +0000

    retirando esto que da problemas

--- init.el
+++ init.el
@@ -34,11 +34,6 @@
 (setq echo-keystrokes 0.02
       show-paren-delay 0)
 
-;; Usar utf-8
-(set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-keyboard-coding-system 'utf-8-unix)
-
 ;; Sobreescribir la región seleccionada
 (delete-selection-mode 1)
 
